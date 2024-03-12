084261e98b0d575162ed8e42b86b2e92b08ea1fa
Kartikey Singh <codingwalaid@gmail.com>
2024-03-12 16:57:42 +0530

    Fix image issue

--- api/server.js
+++ api/server.js
@@ -60,15 +60,19 @@ app.post("/uploadImage", upload.single("image"), async (req, res) => {
     await s3.upload(params).promise();
 
     // Generate a signed URL for the uploaded image
-    const urlParams = {
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: key,
-      Expires: 0,
-    };
+    // const urlParams = {
+    //   Bucket: process.env.AWS_S3_BUCKET_NAME,
+    //   Key: key,
+    //   Expires: 0,
+    // };
 
-    const signedURL = s3.getSignedUrl("getObject", urlParams);
+    // const signedURL = s3.getSignedUrl("getObject", urlParams);
 
-    res.status(200).json({ success: true, url: signedURL });
+    // res.status(200).json({ success: true, url: signedURL });
+    const objectUrl = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
+
+    res.status(200).json({ success: true, url: objectUrl });
+    
   } catch (err) {
     console.error("Error uploading image:", err);
     res.status(500).json({ success: false, message: "Failed to upload image" });
