7807b6fff630828b33601f4b117bd7f1d8a958e3
Kartikey Singh <codingwalaid@gmail.com>
2024-04-05 01:29:08 +0530

    Added background image to Organization dashboard

--- client/src/pages/dashOrg/DashOrg.jsx
+++ client/src/pages/dashOrg/DashOrg.jsx
@@ -20,6 +20,7 @@ export default function DashOrg({ user }) {
                     </Link>
                 </div>
             </div>
+            <img className="background-stripe" src="images/backdrop.png" alt="backrop" />
         </div>
     );
 }
